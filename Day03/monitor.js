a8205641ed1cc09522e593b0122dc2cb1ba6d27e
Aeagon07 <godaserushikesh07@gmail.com>
2025-07-01 16:58:59 +0530

    Day 3

---
+++ Day03/monitor.js
@@ -0,0 +1,56 @@
+// Monitoring System
+
+// FUnctinality => cpu core info, ram info
+
+// Note => If you want a function recursly or again an again then use the setInterval method
+import chalk from 'chalk';
+import os from 'node:os'
+
+
+function monitor(){
+    console.log('Monitoring...')
+    // Take a snapshot
+
+    // Take another snapshot after a second
+
+    const oldCpus = os.cpus();
+
+
+    setTimeout(()=>{
+        const newCpu = os.cpus();
+
+        const usage = newCpu.map((cpu, i) => {
+            return{
+                core: i,
+                usage: calculateCPU(oldCpus[i], newCpu[i]) + '%',
+            };
+        });
+
+        setTimeout(() => {
+            console.clear()
+        }, 1000)
+
+        console.table(usage)
+
+        const useMem = ((os.totalmem() - os.freemem()) / (1024 * 1024 * 1024)).toFixed(2);
+        const totalMem = (os.totalmem() / (1024 * 1024 * 1024)).toFixed(2);
+        console.log('Memory Used: ', 
+            useMem > 6.9 ? chalk.redBright(`${useMem} / ${totalMem} GB`)
+            : chalk.greenBright(`${useMem} / ${totalMem} GB`)
+        );
+    }, 1000)
+}
+
+function calculateCPU(oldCpu, newCpu){
+    const oldTotal = Object.values(oldCpu.times).reduce((a, b) => a + b);
+    const newTotal = Object.values(newCpu.times).reduce((a, b) => a + b);
+
+    const idle = newCpu.times.idle - oldCpu.times.idle
+
+    const total = newTotal - oldTotal;
+    
+    const used = total - idle;
+    return ((used * 100)/ total).toFixed(1);
+// toFixed() for avoiding the decimal points
+}
+setInterval(monitor, 1000);
